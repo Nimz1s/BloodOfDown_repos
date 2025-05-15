409d032c3617d3f0e1fb5b3c48eb80ecdb5ac076
Nimz1s <vozniukillia068@gmail.com>
2025-05-15 20:01:41 +0300

    some frik

---
+++ Source/BloodOfDown/Public/BTService_ChangeSpeed.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_ChangeSpeed.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOODOFDOWN_API UBTService_ChangeSpeed : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
